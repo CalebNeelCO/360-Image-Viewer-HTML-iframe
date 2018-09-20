7900eb53e54b752252fadb479877cb5fe830f69e
Caleb Neel <me@calebneel.com>
2018-09-20 12:44:14 -0600

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__,static_url_path='')
 
 @app.route('/')
 def send_home():
-	return return render_template('index.html',tag ="test")
+	return render_template('index.html',tag ="test")
 
 
 if __name__ == '__main__':
